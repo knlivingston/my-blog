bedb0829e7cada92b7b5993059a89142d13c3329
Kimberly Livingston <kimnlivingston@gmail.com>
2020-05-07 20:06:30 -0400

    First Commit

---
+++ src/build/precache-manifest.0affaa6296ff3babf189abd20bcc9942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9a457922650c10873c",
+    "url": "/static/css/main.59b4e5a0.chunk.css"
+  },
+  {
+    "revision": "6d9a457922650c10873c",
+    "url": "/static/js/main.8a33d725.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3411b17d3915443b45c",
+    "url": "/static/js/2.4a7f71bf.chunk.js"
+  },
+  {
+    "revision": "74dc72f7d54509d0521b13652069257a",
+    "url": "/index.html"
+  }
+];
